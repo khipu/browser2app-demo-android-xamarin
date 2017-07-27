fb273dba7f05e245aca0adff6f55bd2002dc46ad
Emilio Davis <emilio.davis@gmail.com>
2017-07-27 18:25:01 -0400

    initial commit

---
+++ DemoBrowser2AppXamarin/DemoBrowser2AppXamarinApplication.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Browser2app.Khenshin;
+
+namespace DemoBrowser2AppXamarin
+{
+    [Application]
+    public class DemoBrowser2AppXamarinApplication : Application, IKhenshinApplication
+    {
+
+        public static DemoBrowser2AppXamarinApplication Current { get; set; }
+
+        public IKhenshinInterface Khenshin { get; set; }
+
+        public DemoBrowser2AppXamarinApplication(IntPtr handle, global::Android.Runtime.JniHandleOwnership transfer)
+        : base(handle, transfer)
+        {
+
+		}
+		public override void OnCreate()
+		{
+			base.OnCreate();
+			Current = this;
+            Khenshin  = new Khenshin.KhenshinBuilder()
+						.SetApplication(this)
+						.SetTaskAPIUrl("https://cmr.browser2app.com/api/automata/")
+						.SetDumpAPIUrl("https://cmr.browser2app.com/api/automata/")
+						.SetMainButtonStyle(1)
+						.SetAutomatonTimeout(90)
+						.SetAllowCredentialsSaving(true)
+						.SetHideWebAddressInformationInForm(false)
+						.Build();
+		}
+
+
+
+    }
+}
---
+++ DemoBrowser2AppXamarin/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Com.Browser2app.Khenshin;
+
+namespace DemoBrowser2AppXamarin
+{
+    [Activity(Label = "DemoBrowser2AppXamarinApplication", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate {
+
+
+
+                var paymentIntent = DemoBrowser2AppXamarinApplication.Current.Khenshin.StartTaskIntent;
+                paymentIntent.PutExtra(KhenshinConstants.ExtraAutomatonId, "Bawdf");
+
+                var bundle = new Bundle();
+				bundle.PutString("subject", "Pago prueba");
+				bundle.PutString("amount", "2000");
+				bundle.PutString("merchant", "Comercio de prueba");
+				bundle.PutString("paymentId", "aosjdoaijsdoiaj");
+				bundle.PutString("khipu_account_name", "PAGOCMR");
+				bundle.PutString("khipu_account_number", "55200104571");
+				bundle.PutString("khipu_alias", "PAGOCMR");
+				bundle.PutString("payer_name", "Emilio Davis");
+				bundle.PutString("payer_email", "cmr@khipu.com");
+				bundle.PutString("khipu_rut", "10.706.077-4");
+				bundle.PutString("khipu_email", "transferencias@khipu.com");
+
+                paymentIntent.PutExtra(KhenshinConstants.ExtraAutomatonParameters, bundle);
+
+                StartActivityForResult(paymentIntent, 101);
+
+            
+            };
+        }
+    }
+}
+
